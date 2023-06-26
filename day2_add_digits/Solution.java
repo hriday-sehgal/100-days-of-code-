1f8257b7788039d52d026ebc8169069802cd469e
Hriday Sehgal <102664313+hriday-sehgal@users.noreply.github.com>
2023-06-26 12:43:32 +0530

    Create Solution.java

---
+++ day2_add_digits/Solution.java
@@ -0,0 +1,10 @@
+//258. Add Digits
+// Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+class Solution {
+    public int addDigits(int num) {
+        if(num ==0) return 0;
+        if(num % 9 == 0) return 9;
+        return num % 9;
+        
+    }
+}
