a93b244ceeb700178e0c9e026956ad3d9f975034
Hriday Sehgal <102664313+hriday-sehgal@users.noreply.github.com>
2023-06-26 12:45:06 +0530

    Create Solution.java

---
+++ day3_tolowercase/Solution.java
@@ -0,0 +1,8 @@
+//709. To Lower Case
+//Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+class Solution {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
